828bf70121db0ffb9195511040265c02e7f7a959
Mario H. Adaniya <mhadaniya@gmail.com>
2020-07-19 20:34:25 -0300

    :pencil2: Arrumando o nome da migrations

--- database/migrations/2020_07_19_201100_add_price_column_stock_mutations_table.php
+++ database/migrations/2020_07_19_201100_add_price_column_stock_mutations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStockMutationsTable extends Migration
+class AddPriceColumnStockMutationsTable extends Migration
 {
     /**
      * Run the migrations.
